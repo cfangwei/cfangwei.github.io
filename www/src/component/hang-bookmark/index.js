43f323c3ce0b285a07d4b3938913ed8ec7a8f780
AbyChan <crystal.tyan@gmail.com>
2016-04-15 11:32:27 +0800

    update

---
+++ www/src/component/hang-bookmark/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var $ = require('jquery');
+
+module.exports = {
+    template: require('./template.html'),
+    //props: ['githubId', 'name', 'state', 'time', 'chineseName'],
+    // data: function(){
+    //     return {
+    //         show: 'block'
+    //     };
+    // },
+    ready: function(){
+        
+    }
+};
